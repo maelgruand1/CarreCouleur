4048a49a98d679b8f2ea29b3916fca2109807d24
Maël Gruand <maelgruand1@yahoo.com>
2025-05-30 18:07:04 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-var pages = ["index.html", "game.html"];
-var choice = ["Computer", "IA", "Payer"];
-var current;
-// Redirige à la page game.html
-function goGame() {
-  window.location.href = pages[1];
-  alert("Redirige au jeu");
-  console.log("Redirect to ", pages[1]);
-}
